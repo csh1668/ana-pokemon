92ce3f235cc50bafb979a6e896ad1fe7cdd1a55e
SeoHyeon <tjgus1668@naver.com>
2024-09-20 13:59:41 +0900

    Initial commit

---
+++ pokemon-be/src/main/java/kr/anacnu/pokemonbe/PokemonBeApplication.java
@@ -0,0 +1,13 @@
+package kr.anacnu.pokemonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonBeApplication.class, args);
+	}
+
+}
---
+++ pokemon-be/src/test/java/kr/anacnu/pokemonbe/PokemonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.anacnu.pokemonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
