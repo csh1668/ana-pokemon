b582bcd937c81d3af1ed25ef27d0fe262a8fd758
factwy <202102700@o.cnu.ac.kr>
2024-09-21 21:44:15 +0900

    frontend - 포켓몬 도감 테이블 작성

---
+++ pokemon-fe/src/views/data.js
@@ -0,0 +1,101 @@
+export default [
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    },
+    {
+        "img": "https://i.namu.wiki/i/i1owLjzrlP6NVe-WUYNYNZhz0-iWPrV1gwUFm65hTSbtz53xGL9ewT83XxFvIRIydsKjz8EhP6zUccINek5fZA.webp",
+        "name": "namename",
+        "number": "No.1234",
+        "type": "stone",
+        "ability": "none",
+        "height": "20m",
+        "weight": "100kg"
+    }
+]
