0a8a6d53d4fc1f7c6bf7ee7d7da70e1c66903385
SeoHyeon <tjgus1668@naver.com>
2024-09-22 15:28:51 +0900

    frontend - 포켓몬 도감 테이블 작성
    변경 사항이 터졌어서 다시 추가합니다

--- pokemon-fe/src/App.js
+++ pokemon-fe/src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './views/data'
 
 function App() {
+  const dt = data
+  console.log("data : ", dt)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button">
+        <img src="https://cdn-icons-png.flaticon.com/512/159/159833.png" width="50" height="50"/>
+      </button>
+        <table>
+            <tr>
+                <td><select>
+                    <option>타입</option>
+                    <option>특성</option>
+                </select></td>
+                <td><input type="text"/></td>
+                <td><input type="submit" value="검색"/></td>
+            </tr>
+        </table>
+        <table id="pokemon_table">
+          <thead>
+            <tr>
+              <td>img</td>
+              <td>name</td>
+              <td>number</td>
+              <td>type</td>
+              <td>ability</td>
+              <td>height</td>
+              <td>weight</td>
+            </tr>
+          </thead>
+          <tbody>
+            {dt.map((d)=>(
+              <tr key={d.number}>
+                <td>
+                  <img src={d.img} width="50" height="50"></img>
+                </td>
+                <td>{d.name}</td>
+                <td>{d.number}</td>
+                <td>{d.type}</td>
+                <td>{d.ability}</td>
+                <td>{d.height}</td>
+                <td>{d.weight}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </div>
   );
 }
