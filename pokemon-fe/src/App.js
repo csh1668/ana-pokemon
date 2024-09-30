3875313dae947f7cdacd5e5d6ce52200a0b5a9fb
factwy <202102700@o.cnu.ac.kr>
2024-09-30 22:27:04 +0900

    React - Login 화면 재구성

--- pokemon-fe/src/App.js
+++ pokemon-fe/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 import React, { useEffect, useState, useRef, useCallback } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import PokemonDetail from './PokemonDetail.js'
@@ -8,6 +8,10 @@ function App() {
   const [visiblePokemons, setVisiblePokemons] = useState([])  // 현재 화면에 보여줄 포켓몬
   const [page, setPage] = useState(0)  // 현재 페이지
   const observerRef = useRef()  // IntersectionObserver ref
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [userId, setUserId] = useState('')
+  const [password, setPassword] = useState('')
 
   // Backend API ('/list') 를 통한 초기 데이터 획득
   useEffect(()=>{
@@ -50,35 +54,45 @@ function App() {
     if (node) observerRef.current.observe(node)
   }, [loadMorePokemons, visiblePokemons.length])
 
-  const loginPrompt = ()=>{
-    const userId = prompt('ID를 입력해주세요.')
-    if (userId === null) return;
+    // 팝업 열기/닫기
+    const togglePopup = () => {
+      setIsOpen(!isOpen)
+    }
 
-    const userPw = prompt('PW를 입력해주세요.')
-    if (userPw === null) return;
-
-    axios({
-      method: "POST",
-      url: `/proxy/sign-in`,
-      data: {
-        studentId: userId,
-        password: userPw
+    // 팝업 바깥을 클릭했을 때 닫기
+    const closePopup = (e) => {
+      if (e.target.className === 'popup') {
+        setIsOpen(false)
       }
-    })
-    .then((res)=>res.data)
-    .then((res)=>{
-      console.log(`token : ${res.token}`)
-      console.log(`meesage : ${res.message}`)
-
-      if (res.token) {
-        alert('로그인 성공')
-      } else {
-        alert('로그인 실패')
-      }
-    })
-
-    console.log(`Id : ${userId}, Pw : ${userPw}`)
-  }
+    }
+  
+    // 입력 처리
+    const handleLogin = (e) => {
+      e.preventDefault()
+      // 로그인 로직 작성
+      axios({
+        method: "POST",
+        url: `/proxy/sign-in`,
+        data: {
+          studentId: userId,
+          password: password
+        }
+      })
+      .then((res)=>res.data)
+      .then((res)=>{
+        console.log(`token : ${res.token}`)
+        console.log(`meesage : ${res.message}`)
+  
+        if (res.token) {
+          alert('로그인 성공')
+        } else {
+          alert('로그인 실패')
+        }
+      })
+  
+      console.log(`Id : ${userId}, Pw : ${password}`)
+      setIsOpen(false) // 팝업 닫기
+    }
 
   return (
     <Router>
@@ -86,8 +100,43 @@ function App() {
         {/* 메인 페이지 */}
         <Route path="/" element = {
           <div className="appContainer">
-            <button type="button" className="Button" onClick={loginPrompt}>
-              <img src="https://cdn-icons-png.flaticon.com/512/159/159833.png" width="50" height="50"/>
+            {/* <button type="button" className="Button" onClick={loginPrompt}> */}
+            <button type='button' className='Button' onClick={togglePopup}>
+              {(isOpen && (
+                <div className="popup" onClick={closePopup}>
+                  <div className="popup-inner" onClick={(e)=> e.stopPropagation()}>
+                    <h2>Login</h2>
+                    <form onSubmit={handleLogin}>
+                      <label>
+                        아이디 : 
+                        <input
+                          type="text"
+                          value={userId}
+                          onChange={(e) => setUserId(e.target.value)}
+                          required
+                        />
+                      </label>
+                      <br />
+                      <label>
+                        비밀번호 : 
+                        <input
+                          type="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                        />
+                      </label>
+                      <br />
+                      <button type="submit">로그인 하기</button>
+                      <button type="button" onClick={togglePopup}>
+                        뒤로 가기
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              )) || (
+                <img src="https://cdn-icons-png.flaticon.com/512/159/159833.png" width="50" height="50"/>
+              )}
             </button>
 
             <div className='searchBar'>
@@ -112,7 +161,7 @@ function App() {
                           <p className="pokemonName">{pokemonInfo.name}</p>
                         </div>
                       </Link>
-                    );
+                    )
                   } else {
                     return (
                       <Link to={`/pokemon/${pokemonId}`} key={pokemonInfo.pokedexNum}>
@@ -122,7 +171,7 @@ function App() {
                           <p className="pokemonName">{pokemonInfo.name}</p>
                         </div>
                       </Link>
-                    );
+                    )
                   }
                 })}
               </div>
@@ -135,7 +184,7 @@ function App() {
         <Route path="/pokemon/:id" element={<PokemonDetail pokeList = {visiblePokemons}/>}></Route>
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
